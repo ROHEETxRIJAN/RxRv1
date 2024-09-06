af4532d8e4471b932e353c7ae241945165038005
ROHEETxRIJAN <roheetboss@gmail.com>
2024-09-06 22:20:07 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os, platform
- os.system('git pull')
+ 
+print('\033[1;37m\u001b[31m>>\033[0m\033[1;37m WAIT SOMETIME...!');os.system('git pull');print('')
 bit = platform.architecture()[0]
 if bit == '32bit':
-    import RxR32
+    import NEPAL32
 elif bit == '64bit':
-    import RxR64
+    import NEPAL64
 else: print(" YOUR DEVICE DOESN'T SUPPORT MY TOOL");exit()
+ 
