c4d05860bdefea0fc450fecce5ab62695a5f2fdc
ROHEETxRIJAN <roheetboss@gmail.com>
2024-10-05 20:59:56 +0545

    Create rxr.py

---
+++ rxr.py
@@ -0,0 +1,20 @@
+import os, platform, time, sys
+os.system('xdg-open https://chat.whatsapp.com/GKCh3wB9KoU9oDuy0MsZ7v')
+try:
+ import requests
+except:os.system("pip install requests")
+print('\033[1;37m\u001b[31m>>\033[0m\033[1;37m CHECKING FOR UPDATEz...')
+os.system('git pull --quiet ')
+bit = platform.architecture()[0]
+if bit == "64bit":
+ print('\033[1;37m\u001b[31m>>\033[0m\033[1;37m 64BIT FOUND')
+ time.sleep(2)
+ import rxr
+
+ 
+ 
+ 
+if bit == "32bit":
+ print('\033[1;37m\u001b[31m>>\033[0m\033[1;37m 32BIT FOUND')
+ time.sleep(2)
+ print('\033[1;37m\u001b[31m>>\033[0m\033[1;37m 32BIT NOT SUPPORTED')
